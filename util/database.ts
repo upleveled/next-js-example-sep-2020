6f8da60484f7a82965431f69b36ef5938046bf61
Karl Horky <karl.horky@gmail.com>
2020-10-14 12:42:14 +0200

    Connect via SSL to Heroku PostgreSQL

--- util/database.ts
+++ util/database.ts
@@ -8,7 +8,13 @@ extractHerokuDatabaseEnvVars();
 
 dotenv.config();
 
-const sql = postgres();
+const sql =
+  process.env.NODE_ENV === 'production'
+    ? // Heroku needs SSL connections but
+      // has an "unauthorized" certificate
+      // https://devcenter.heroku.com/changelog-items/852
+      postgres({ ssl: { rejectUnauthorized: false } })
+    : postgres();
 
 // If you want to use the connection string instead for testing,
 // you can try this:
