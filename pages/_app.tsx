e52510232a79009071a0f6f5f4ced26c1243e530
Karl Horky <karl.horky@gmail.com>
2020-11-01 16:44:40 +0100

    Fix missing React import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { NextPage } from 'next';
 import globalStyles from '../components/GlobalStyles';
 
--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState } from 'react';
 import { GetServerSidePropsContext } from 'next';
 import Head from 'next/head';
